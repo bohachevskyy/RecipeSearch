005957e954a7f7de2b5f0c6f657231a17d35d535
Mark Vasiv <mark.vasiv@gmail.com>
2019-05-30 18:11:46 +0200

    Add HTML decoding helper

---
+++ RecipeSearch/Helpers/String+HTML.swift
@@ -0,0 +1,28 @@
+//
+//  String+HTML.swift
+//  RecipeSearch
+//
+//  Created by Mark Vasiv on 30/05/2019.
+//  Copyright © 2019 Mark Vasiv. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func stringByDecodingHTML() -> String? {
+        guard let data = self.data(using: .utf8) else {
+            return nil
+        }
+        
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+        
+        guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
+            return nil
+        }
+        
+        return attributedString.string
+    }
+}
